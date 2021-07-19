9c4d317ac3f56c7f8474b4b805bbce1fac434cd5
phatlt <phatlt2908@gmail.com>
2021-07-20 01:31:42 +0700

    update Access-Control

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const app = express();
 // parse requests of content-type - application/json
 app.use(express.json());
 
-app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "*");
-	res.header("Access-Control-Allow-Methods", "*");
-	next();
-});
+// app.use(function (req, res, next) {
+// 	res.header("Access-Control-Allow-Origin", "*");
+// 	res.header("Access-Control-Allow-Headers", "*");
+// 	res.header("Access-Control-Allow-Methods", "*");
+// 	next();
+// });
+
+var cors = require('cors');
+app.use(cors({ origin: true, credentials: true }));
 
 // Read environment variable
 require('dotenv').config();
